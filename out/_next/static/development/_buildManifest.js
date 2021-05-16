e334524b1f1f211c268794c486a7a6e7b3752db2
RoMeAh <runa.ronit3129@gmail.com>
2021-05-16 17:14:10 +0530

    ✅ build: manually build as gh actions is down

---
+++ out/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/blogs/[blog]":["static\u002Fchunks\u002Fpages\u002Fblogs\u002F[blog].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fblogs\u002F[blog]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
